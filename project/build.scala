ea6caf1a623f45fae6e536a7270bf4f3f82cc3a6
Garrett Eidsvig <geidsvig@172-18-11-244.ip.hootsuitemedia.com>
2012-08-20 15:52:41 -0700

    Using kaesler's branch as reference, unit tests now pass. Updated dependencies to akka 2.0.2, sbt 0.11.3. Versioned to 2.1.0 as I do not intend to do further work on this branch.

--- project/build.scala
+++ project/build.scala
@@ -7,8 +7,8 @@ object build extends Build {
 	lazy val standardSettings = Defaults.defaultSettings ++ Seq(
 		resolvers += "Typesafe Repo" at "http://repo.typesafe.com/typesafe/releases/",
 		organization := "com.github.geidsvig",
-		version := "2.1-SNAPSHOT",
-		scalaVersion := "2.9.1-1")
+		version := "2.1.0",
+		scalaVersion := "2.9.2")
 
 	lazy val root = Project(
 		id = "akka-amqp",
@@ -17,7 +17,6 @@ object build extends Build {
 			libraryDependencies ++= Seq(
 				AmqpClient,
 				AkkaActor,
-				Specs2,
 				JUnit,
 				ScalaTest,
 				AkkaTestKit,
--- project/dependencies.scala
+++ project/dependencies.scala
@@ -3,11 +3,10 @@ import sbt._
 object dependencies {
 
 	def AmqpClient = "com.rabbitmq" % "amqp-client" % "2.8.2"
-	def AkkaActor = "com.typesafe.akka" % "akka-actor" % "2.0.1"
-	def Specs2 = "org.specs2" %% "specs2" % "1.9" % "test"
+	def AkkaActor = "com.typesafe.akka" % "akka-actor" % "2.0.2"
 	def JUnit = "junit" % "junit" % "4.7" % "test"
-	def ScalaTest = "org.scalatest" %% "scalatest" % "1.7.1" % "test"
-	def AkkaTestKit = "com.typesafe.akka" % "akka-testkit" % "2.0.1" % "test"
+	def ScalaTest = "org.scalatest" % "scalatest_2.9.0" % "1.8" % "test"
+	def AkkaTestKit = "com.typesafe.akka" % "akka-testkit" % "2.0.2" % "test"
 	def Mockito = "org.mockito" % "mockito-all" % "1.9.0" % "test"
 	
 }
